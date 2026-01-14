220d46e9c22acd9cc251e0bd012595f0ac27eb46
eris91 <52816361+eris91@users.noreply.github.com>
2026-01-14 14:06:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.set_page_config(page_title="PENMADARC", layout="wide")
+st.set_page_config(page_title="ARSIP DIGITAL PENMAD", layout="wide")
 
 # Root langsung ke Dashboard multipage
 st.switch_page("pages/1_Dashboard.py")
+
